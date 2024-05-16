b89eda5fc4ca1c584bc12d5966110ef602f79367
Mahendra <mahendra.nayak@datagrokr.com>
2024-05-16 15:06:36 +0530

    generalize the code

--- api_helper.py
+++ api_helper.py
@@ -1,27 +1,17 @@
 import requests
 import logging
-
+from psycopg2 import Error
 class APIFetch:
     def __init__(self) -> None:
         pass
     def fetch_data(self,url,country:None):
-        if country is not None:
-            print("Data of Country and Univerities:")
+        try:
+            if country is not None:
+                url=url+f"country={country}"
             response = requests.get(url)
             if response.status_code == 200:
-                universities = response.json()
-                print(country,universities)
-            else:
-                logging.error(f"Failed to fetch data for {country}")
-        else:
-            print("data of persons:")
-            for _ in range(50): 
-                response = requests.get("https://randomuser.me/api/")
-                if response.status_code == 200:
-                    persons = response.json()['results']
-                print(persons)
-            else:
-                logging.error("Failed to fetch data for persons")
-
-def main():
-    countries=[]
+                return response.json()        
+        except Error as e:
+            logging.error(f"Failed to fetch data for {url}",e)
+    
+           
--- driver.py
+++ driver.py
@@ -1,23 +1,16 @@
 
 from api_helper import APIFetch
-# class PersonDataFetcher():
-
-#     def fetch_data(self):
-#         for _ in range(5000): 
-#             response = requests.get("https://randomuser.me/api/")
-#             if response.status_code == 200:
-#                 persons = response.json()['results']
-#                 print(persons)
-#             else:
-#                 logging.error("Failed to fetch data for persons")
 
 def main():
-    # countries = ["india", "usa", "uk", "canada", "australia", "germany", "france", "italy", "spain", "japan", "china", "brazil", "russia", "south africa", "mexico", "south korea", "netherlands", "switzerland", "sweden", "belgium"]
-    # loader1=APIFetch()
-    # for country in countries:
-    #     url=f"http://universities.hipolabs.com/search?country={country}"
-    #     loader1.fetch_data(url,country)
+    countries = ["india", "usa", "uk", "canada", "australia", "germany", "france", "italy", "spain", "japan", "china", "brazil", "russia", "south africa", "mexico", "south korea", "netherlands", "switzerland", "sweden", "belgium"]
+    loader1=APIFetch()
+    for country in countries:
+
+        answer=loader1.fetch_data("http://universities.hipolabs.com/search?country",country)
+        print(country,answer)
     loader2=APIFetch()
-    loader2.fetch_data("https://randomuser.me/api/",None)
+    for _ in range(50): 
+       person=loader2.fetch_data("https://randomuser.me/api/",None)
+       print(person)
 if __name__=="__main__":
     main()
