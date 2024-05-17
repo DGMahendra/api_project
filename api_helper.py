20eee66a0911e00f022b44cae4dd31927e641e89
Mahendra <mahendra.nayak@datagrokr.com>
2024-05-17 16:49:36 +0530

    api data loaded to database

--- api_helper.py
+++ api_helper.py
@@ -1,17 +1,79 @@
-import requests
+import time
 import logging
+import psycopg2
+import requests
 from psycopg2 import Error
+
+def log_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        logging.info(f"{func.__name__} took {end_time - start_time} seconds to complete.")
+        return result
+    return wrapper
+
 class APIFetch:
-    def __init__(self) -> None:
-        pass
-    def fetch_data(self,url,country:None):
+    def __init__(self, connection, cursor):
+        self.connection = connection
+        self.cursor = cursor
+
+    @log_time
+    def fetch_data(self, url, country=None):
         try:
             if country is not None:
-                url=url+f"country={country}"
+                url = url + f"?country={country}"
             response = requests.get(url)
             if response.status_code == 200:
-                return response.json()        
+                return response.json()
         except Error as e:
-            logging.error(f"Failed to fetch data for {url}",e)
+            logging.error(f"Failed to fetch data for {url}: {e}")
+
+    def close_connection(self):
+        if self.cursor is not None and not self.cursor.closed:
+            self.cursor.close()
+        if self.connection is not None and self.connection.closed == 0:
+            self.connection.close()
+
+    @log_time
+    def insert_data(self, data_to_insert):
+        try:
+            inserted_data = []
+            for table_name, data_list in data_to_insert.items():
+                for data in data_list:
+                    if table_name == 'country':
+                        country_id = self.insert_country(data['country'])
+                        inserted_data.append((country_id, table_name))
+                    else:
+                        placeholders = ', '.join(['%s'] * len(data))
+                        columns = ', '.join(data.keys())
+                        values = tuple(data.values())
+                        query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders}) ON CONFLICT DO NOTHING"
+                        self.cursor.execute(query, values)
+                        self.connection.commit()
+                        inserted_data.append((None, table_name))
+            return inserted_data
+        except Error as e:
+            self.connection.rollback()  # Rollback the transaction
+            logging.error(f"Error while inserting data: {e}")
+            return [("Error occurred", None)]
+        except psycopg2.InterfaceError as e:
+            logging.error(f"InterfaceError occurred: {e}")
+            return [("InterfaceError occurred", None)]
+        finally:
+            if self.cursor is not None and not self.cursor.closed:
+                self.cursor.close()     # Close the cursor if it's not already closed
+            if self.connection is not None and self.connection.closed == 0:
+                self.connection.close() # Close the connection if it's not already closed
+    @log_time
+    def insert_country(self, country_name):
+        try:
+            query = "INSERT INTO country (country_name) VALUES (%s) ON CONFLICT DO NOTHING RETURNING country_id"
+            self.cursor.execute(query, (country_name,))
+            country_id = self.cursor.fetchone()[0]
+            return country_id
+        except Error as e:
+            logging.error("Error while inserting country", e)
+            return None
+
     
-           
--- driver.py
+++ driver.py
@@ -1,16 +1,48 @@
+import logging
+
+import psycopg2
 
 from api_helper import APIFetch
 
-def main():
-    countries = ["india", "usa", "uk", "canada", "australia", "germany", "france", "italy", "spain", "japan", "china", "brazil", "russia", "south africa", "mexico", "south korea", "netherlands", "switzerland", "sweden", "belgium"]
-    loader1=APIFetch()
-    for country in countries:
 
-        answer=loader1.fetch_data("http://universities.hipolabs.com/search?country",country)
-        print(country,answer)
-    loader2=APIFetch()
-    for _ in range(50): 
-       person=loader2.fetch_data("https://randomuser.me/api/",None)
-       print(person)
+def main():
+    connection = psycopg2.connect(database="api_database", user="postgres", password="Password@186", host="localhost", port="5432")
+    cursor = connection.cursor()
+
+    # Create an instance of APIFetch with the connection and cursor
+    loader = APIFetch(connection, cursor)
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    countries = ["india", "usa", "uk", "canada", "australia", "germany", "france", "italy", "spain", "japan", "china", "brazil", "russia", "south africa", "mexico", "south korea", "netherlands", "switzerland", "sweden", "belgium"]
+   
+    data_to_insert = {"country": countries}
+    inserted_country_data = loader.insert_data(data_to_insert)
+
+    for log_message in inserted_country_data:
+        logging.info(log_message)
+    
+    # Fetch university data for each country
+    universities_data = {}
+    for country in countries:
+        university_data = loader.fetch_data("http://universities.hipolabs.com/search", country)
+        universities_data[country] = university_data
+    
+    # Insert university data
+    data_to_insert["university"] = universities_data
+    inserted_university_data = loader.insert_data(data_to_insert)
+
+    for log_message in inserted_university_data:
+        logging.info(log_message)
+
+    # Fetch person data
+    persons_data = loader.fetch_data("https://randomuser.me/api/")
+    
+    # Insert person data
+    data_to_insert["person"] = persons_data['results']
+    inserted_person_data = loader.insert_data(data_to_insert)
+
+    for log_message in inserted_person_data:
+        logging.info(log_message)
+
+    loader.close_connection()
 if __name__=="__main__":
     main()
